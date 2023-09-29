d1add2e199763e28f1c3c5e48c2e3727be3c6299
Fargol Shirvani Far <fargolsh79@gmail.com>
2023-09-29 13:27:01 +0330

    Add Billing, CardDeal, FeedbackCard, and Testimonial sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Button = ({ styles }) => {
   return (
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins
-    font-medium text-[18px] text-primary outline-none rounded-[10px] hover:scale-[1.07] duration-200 ${styles}`}>
+    font-medium text-[18px] text-primary outline-none rounded-[10px] hover:scale-[1.07] 
+    duration-200 ${styles}`}>
       Get Started
     </button>
   )
